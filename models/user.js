1738b99689e0bee4add7d16e46f6f32641445769
Danny Sivyolo <dansivyolo@gmail.com>
2025-03-04 14:13:35 +0200

    Remove to Cart

--- models/user.js
+++ models/user.js
@@ -40,6 +40,15 @@ userSchema.methods.addToCart = function(product) {
     return this.save();
 }
 
+userSchema.methods.removeFromCart = function(productId) {
+    const updatedItems = this.cart.items.filter(item => {
+        return item.productId.toString() !== productId.toString();
+    });
+
+    this.cart.items = updatedItems;
+    return this.save();
+}
+
 module.exports = mongoose.model('user', userSchema);
 
 // const mongodb = require('mongodb');
